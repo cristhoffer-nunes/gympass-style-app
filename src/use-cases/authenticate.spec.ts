6e02f24d180b49c9a6a29ae3a90a6dea186264f4
Cristhoffer Santos <cristhoffer.santos@jbq.global>
2025-08-15 22:53:16 -0300

    feat: refactor test instances

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -3,19 +3,27 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { beforeEach } from 'vitest'
+
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
 
 describe('Authenticate Use Case', () => {
+
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new AuthenticateUseCase(usersRepository)
+
+    })
+
     it('should be able to authenticate', async () => {
 
-        const usersRepository = new InMemoryUsersRepository()
-        const sut = new AuthenticateUseCase(usersRepository)
 
         await usersRepository.create({
             name: 'John Doe',
             email: 'john.doe@email.com',
             password_hash: await hash('password', 6)
         })
-
         const { user } = await sut.execute({
             email: 'john.doe@email.com',
             password: 'password'
@@ -24,21 +32,12 @@ describe('Authenticate Use Case', () => {
         expect(user.id).toEqual(expect.any(String))
     })
     it('should not be able to authenticate with wrong email', async () => {
-
-        const usersRepository = new InMemoryUsersRepository()
-        const sut = new AuthenticateUseCase(usersRepository)
-
-
         await expect(sut.execute({
             email: 'john.doe@email.com',
             password: 'password'
         })).rejects.toBeInstanceOf(InvalidCredentialsError)
     })
     it('should not be able to authenticate with wrong password', async () => {
-
-        const usersRepository = new InMemoryUsersRepository()
-        const sut = new AuthenticateUseCase(usersRepository)
-
         await usersRepository.create({
             name: 'John Doe',
             email: 'john.doe@email.com',
