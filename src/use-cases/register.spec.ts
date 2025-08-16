43dd502cabf395de8de13a732ea35f410461968f
Cristhoffer Santos <cristhoffer.santos@jbq.global>
2025-08-15 21:42:14 -0300

    feat: add first unit test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test } from 'vitest'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { expect, test, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if works', () => {
-    expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email) {
+                return null
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-id',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date(),
+                }
+            }
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: 'John Doe',
+            email: 'john.doe@mail.com',
+            password: 'password'
+        })
+
+        const isPasswordCorrectluyHashed = await compare('password', user.password_hash)
+
+        expect(isPasswordCorrectluyHashed).toBe(true)
+    })
 })
